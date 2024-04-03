27e172bc07c779a591d1d4a78c4e7e31850a857c
Evgeny Chugunnyy <chgenn.x@gmail.com>
2024-04-03 15:37:27 +0300

    FS storage to dir

--- s3docker/reset-server.sh
+++ s3docker/reset-server.sh
@@ -2,7 +2,8 @@
 baseDir="$(dirname $0)"
 cd "$baseDir"
 docker-compose down
-rm -rf ./minio-data || sudo rm -rf ./minio-data
+rm -rf ./minio-data ./data || sudo rm -rf ./minio-data ./data
 docker-compose rm -fv
+mkdir -p ./data
 docker-compose up -d
 
