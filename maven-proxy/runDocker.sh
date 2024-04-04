11f40f1bd33f486bdcc2c4c02c0a2ffa6ee64400
Evgeny Chugunnyy <chgenn.x@gmail.com>
2024-04-04 12:10:06 +0300

    Use latest image

--- maven-proxy/runDocker.sh
+++ maven-proxy/runDocker.sh
@@ -2,4 +2,4 @@
 baseDir="$(dirname $0)"
 cd "$baseDir"
 
-docker run -u 0:0 -v $PWD/artipie.yaml:/etc/artipie/artipie.yml -v $PWD/repo:/var/artipie/repo -v $PWD/data:/var/artipie/data -p8080:8080 -p8082:8082 -p8085:8085 -p8086:8086 -it artipie/artipie:v0.30.14
+docker run -u 0:0 -v $PWD/artipie.yaml:/etc/artipie/artipie.yml -v $PWD/repo:/var/artipie/repo -v $PWD/data:/var/artipie/data -p8080:8080 -p8082:8082 -p8085:8085 -p8086:8086 -it artipie/artipie
